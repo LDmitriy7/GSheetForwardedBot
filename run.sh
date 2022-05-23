99482865f77070a74e4faf7bd1ef14a80db55ebe
levch <levchenko.d.a1998@gmail.com>
2022-05-23 16:23:06 +0300

    .

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
-poetry export -o requirements.txt --without-hashes
-python3 on_startup.py && docker compose build && docker compose up -d
+poetry export -o requirements.txt --without-hashes &&
+  python3 on_startup.py &&
+  docker compose build &&
+  docker compose up
